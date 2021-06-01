5b922bb5c90a04cc10df3310b78e37ac538f0eae
Allan Koech K <lalan2205@gmail.com>
2021-06-01 21:29:58 +0300

    Added pybind11 directory and py files for testing

---
+++ py/main.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == "__main__":
+    print("Running on python side")
+
+    print("Current working directory is: ", os.getcwd())
+
+    print("Ending python code!")
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 
+// pybind11 imports
+#include "../pybind11/include/pybind11/embed.h"
+
+namespace py = pybind11;
+
+void executePy()
+{
+    py::print("Hello, World from Python in C++!"); // use the Python API
+}
+
 // Entry point for the C++ application
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World from C++";
+    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+
+    std::cout << "Hello World from C++" << std::endl;
+
+    executePy();
 
     return 0;
 }
